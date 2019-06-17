20feac8356ffe623809f5067fe9e75b4ff188cb4
Aleksey Afanasiev <afanasiev-sozvezdie@mail.ru>
2019-06-17 08:11:57 +0300

    start working on tests

--- tests.py
+++ tests.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
+"""
 import encryption
-
 def crypro_test():
     msg = "hello world"
     aes = encryption.AESCrypt()
@@ -11,11 +10,33 @@ def crypro_test():
     #print(crypted)
     #print(decrypted)
     assert (decrypted == msg), 'Decryption fail!'
+"""
+
+import client
+import server
+from threading import Thread
+
+TEST_MESSAGE = "Hello, World"
+
+
+def simple_exchange_test():
+    srv = server.Server()
+    server_thread = Thread(target=srv.run)
+    server_thread.start()
+
+    clients = list()
+    clients.append(client.Client(nickname="cli1"))
+    clients.append(client.Client(nickname="cli2"))
+
+    client[0].send(TEST_MESSAGE)
+
+    srv.stop()
+    server_thread.join()
 
 
 if __name__ == "__main__":
     try:
-        crypro_test()
+        simple_exchange_test()
 
     except AssertionError as e:
         print(e)
