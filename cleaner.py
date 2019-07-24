dd53ac29bdb8005dceed3a391631a21cc338d824
AdamFull <logotipick@gmail.com>
2019-07-25 00:09:15 +0300

    release 0.4 alpha
    - Added chat search.
    - Users can now create chats.
    - Implemented saving messages on the client.
    - Implemented server monitoring.
    - Chats on the server no longer use separate classes.

--- cleaner.py
+++ cleaner.py
@@ -3,6 +3,12 @@ from os import remove
 
 if __name__ == "__main__":
     paths = ['Server/Log/', 'Server/Data/', 'Server/__pycache__/', 'Client/config/', 'Client/Log/', 'Client/__pycache__/', '__pycache__/', '.vscode/']
+    files = ['Server/private.pem', 'Server/config.ini']
     for path in paths:
         rmtree(path, ignore_errors=True)
+    for file in files:
+        try:
+            remove(file)
+        except FileNotFoundError as e:
+            print(e)
 
